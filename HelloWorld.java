d4eb606dc323e816f65631d6353be054083cdaa8
Kirdhay <94076975+Kirdhay@users.noreply.github.com>
2021-11-26 18:34:36 +0530

    removesome

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,7 @@ class HelloWorld {
         System.out.println( "Hello Kirdhay!"); 
         System.out.println( "Hello vijay!");
         System.out.println( "Hello sai!");
-        System.out.println( "Hello shirdi!");
-        System.out.println( "Hello to all!");       
-        System.out.println( "Hello to all to chennai!");
-
+       
         
 
     }
