08bcd79c24e8295a045fea60aa451fcf1c722b22
Kirdhay <94076975+Kirdhay@users.noreply.github.com>
2021-11-26 17:15:12 +0530

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 class HelloWorld {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello Kirdhay!" );
+        System.out.println( "Hello Kirdhay!"); 
+        System.out.println( "Hello vijay!");
+        
     }
 }
